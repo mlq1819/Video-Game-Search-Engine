686cd2dfb0687314f8cdc1b6d6878545dec80671
Michael Quinn <mquinn5@binghamton.edu>
2020-08-13 16:34:39 -0400

    working to integrate search functionality

--- Video Game Search Engine/app.py
+++ Video Game Search Engine/app.py
@@ -1,12 +1,20 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-
 	return render_template('index.html', title="Welcome");
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+	if request.method == 'POST':
+		result = request.form
+		#TODO - perform actions on result and/or define items to match the search keywords
+		return render_template("result.html", items = result)
+	else:
+		return index()
 
 if __name__ == '__main__':
 	app.run('localhost',5050) #5050 is what is currently set in the project Debug port number settings
