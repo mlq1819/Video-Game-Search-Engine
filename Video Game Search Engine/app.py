e03ad426324a2c2ffa90fe48059622e791af5419
Michael Quinn <mquinn5@binghamton.edu>
2020-08-13 13:45:40 -0400

    completed basic app.py framework

--- Video Game Search Engine/app.py
+++ Video Game Search Engine/app.py
@@ -2,4 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-	
+@app.route('/')
+@app.route('/home')
+
+def home():
+
+	return "returned home"
+
+if __name__ == '__main__':
+	app.run('localhost',5050)
