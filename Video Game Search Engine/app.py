c0a744bd3953dea018183e89472b24cb1a3dd9f2
Michael Quinn <mquinn5@binghamton.edu>
2020-08-17 14:14:40 -0400

    working on implementing the call to the api

--- Video Game Search Engine/app.py
+++ Video Game Search Engine/app.py
@@ -1,11 +1,15 @@
 
+response = ""
+
+import requests
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title="Welcome");
+	return render_template('index.html', title="Welcome")
 
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
@@ -17,4 +21,11 @@ def result():
 		return index()
 
 if __name__ == '__main__':
-	app.run('localhost',5050) #5050 is what is currently set in the project Debug port number settings
+	response = requests.get("https://www.giantbomb.com/api/games/?api_key=6fe6fb576b0c7bef2364938b2248e1628759508d")
+	print(response.status_code)
+	print(response.text)
+	if response.status_code == 200 or response.status_code == 301:
+		#TODO - send a GET to the API, download the appropriate data, and save it in a managable and workable way
+		app.run('localhost',5050) #5050 is what is currently set in the project Debug port number settings; both must be changed if either is
+	else:
+		print("Failed to run application")
