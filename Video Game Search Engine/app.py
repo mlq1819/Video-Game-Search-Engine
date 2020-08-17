bb36cd255f6deefb774f0dbe93f10d98075fb58f
Michael Quinn <mquinn5@binghamton.edu>
2020-08-17 15:21:10 -0400

    continuing to work on successful integration of API call

--- Video Game Search Engine/app.py
+++ Video Game Search Engine/app.py
@@ -1,5 +1,8 @@
-
-response = ""
+parsed = []
+key="6fe6fb576b0c7bef2364938b2248e1628759508d"
+baseUrl="https://www.giantbomb.com/api/"
+resource="games"
+platforms = [21,9,43]
 
 import requests
 from flask import Flask, render_template, request
@@ -20,12 +23,36 @@ def result():
 	else:
 		return index()
 
+#Should convert information from response and add to parsed
+def parse(response):
+	print("Successfully downloaded data: Code " + response.status_code)
+
+
+
 if __name__ == '__main__':
-	response = requests.get("https://www.giantbomb.com/api/games/?api_key=6fe6fb576b0c7bef2364938b2248e1628759508d")
-	print(response.status_code)
-	print(response.text)
-	if response.status_code == 200 or response.status_code == 301:
-		#TODO - send a GET to the API, download the appropriate data, and save it in a managable and workable way
+	hasnt_failed = True
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # This is chrome, you can set whatever browser you like
+	for platform in platforms:
+		num_results = 100
+		offset = 0
+		while(num_results == 100):
+			url = baseUrl + resource + "/?api_key=" + key + "&format=json"
+			if offset > 0:
+				url = url + "&offset=" + str(offset)
+			if len(platform) > 0:
+				url = url + "&platforms=" + str(platform)
+			#https://www.giantbomb.com/api/games/?api_key=6fe6fb576b0c7bef2364938b2248e1628759508d&format=json&field_list=name,platforms&filter=platforms=21|9|43
+			print(url)
+			response = requests.get(url, headers=headers)
+			num_results = response.number_of_page_results
+			if response.status_code == 200 or response.status_code == 301:
+				parse(response)
+			else:
+				hasnt_failed = False
+				print("Error code " + str(response.status_code))
+				print(response.reason)
+			offset = offset + num_results
+	if hasnt_failed:
 		app.run('localhost',5050) #5050 is what is currently set in the project Debug port number settings; both must be changed if either is
 	else:
 		print("Failed to run application")
